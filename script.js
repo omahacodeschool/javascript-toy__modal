7a2a68e0beabedf761b8d28a0ea91a0e78f9f63b
John Morrow <CodeMorrow@yahoo.com>
2016-07-19 11:39:04 -0500

    completed all tasks

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-// Replace me with your JavaScript!
+window.addEventListener("load", function(){
+
+  var button = document.getElementById("greeting");
+
+  button.addEventListener("click", function(){
+
+  var modal = document.getElementById("modal__box");
+
+  modal.style.visibility = "visible";
+  });
+  
+});
+
+window.addEventListener("load", function(){
+
+  var close = document.getElementById("close");
+
+  close.addEventListener("click", function(){
+
+  var modal = document.getElementById("modal__box");
+
+  modal.style.visibility = "hidden";
+  });
+  
+});
